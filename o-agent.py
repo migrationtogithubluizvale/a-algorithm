4d71f7d235d8e0b5511a8220a07c602c66ee4aef
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2020-03-22 03:30:41 +0000

    The abstract class for an OnlineDFSAgent

---
+++ o-agent.py
@@ -0,0 +1,47 @@
+class OnlineDFSAgent:
+    """
+    The abstract class for an OnlineDFSAgent. Override
+    update_state method to convert percept to state. While initializing
+    the subclass a problem needs to be provided which is an instance of
+    a subclass of the Problem class.
+    """
+
+    def __init__(self, problem):
+        self.problem = problem
+        self.s = None
+        self.a = None
+        self.untried = dict()
+        self.unbacktracked = dict()
+        self.result = {}
+
+    def __call__(self, percept):
+        s1 = self.update_state(percept)
+        if self.problem.goal_test(s1):
+            self.a = None
+        else:
+            if s1 not in self.untried.keys():
+                self.untried[s1] = self.problem.actions(s1)
+            if self.s is not None:
+                if s1 != self.result[(self.s, self.a)]:
+                    self.result[(self.s, self.a)] = s1
+                    self.unbacktracked[s1].insert(0, self.s)
+            if len(self.untried[s1]) == 0:
+                if len(self.unbacktracked[s1]) == 0:
+                    self.a = None
+                else:
+                    # else a <- an action b such that result[s', b] = POP(unbacktracked[s'])
+                    unbacktracked_pop = self.unbacktracked.pop(s1)
+                    for (s, b) in self.result.keys():
+                        if self.result[(s, b)] == unbacktracked_pop:
+                            self.a = b
+                            break
+            else:
+                self.a = self.untried.pop(s1)
+        self.s = s1
+        return self.a
+
+    def update_state(self, percept):
+        """To be overridden in most cases. The default case
+        assumes the percept to be of type state."""
+        return percept
+
